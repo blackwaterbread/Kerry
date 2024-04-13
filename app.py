bcf18a80102cd4d963a71e986e4ee6789f2148ea
dayrain <blackwaterbread@gmail.com>
2024-04-13 11:17:17 +0900

    fixed: initial start error

--- app.py
+++ app.py
@@ -26,7 +26,6 @@ client = discord.Client(intents=intents)
 print('[App] Loading recently posts ...')
 storage: list = load_posts()
 
-
 @client.event
 async def on_ready():
     print(f'[Discord] Logged in as {client.user}')
@@ -37,31 +36,36 @@ async def task():
     new_posts = get_posts()
     last_post = new_posts[-1]
 
-    if last_post['id'] > storage[-1]['id']:
-        channel = await client.fetch_channel(CHANNEL_ID)
-        p = new_posts.index(storage[-1]) + 1
-        # it will be problem when it bigger
-        newer = new_posts[p:]
-        storage.extend(newer)
+    if not storage:
+        storage.extend(new_posts)
         save_posts(storage)
-        print(f'[App] New post detected: {len(newer)}')
-
-        for post in newer:
-            if (type(channel) == discord.channel.TextChannel):
-                image_attached = '[글]' if post['type'] == 'icon_txt' else '[이미지]'
-                embed = discord.Embed(
-                    title = post['subject'],
-                    description = f"{image_attached} {post['username']}",
-                    url = f"${post_url}&no={post['id']}",
-                    color = discord.Color.blue()
-                )
-                await channel.send(embed = embed)
-            else:
-                # todo: Exception
-                return
 
     else:
-        print('[App] No new post')
+        if last_post['id'] > storage[-1]['id']:
+            channel = await client.fetch_channel(CHANNEL_ID)
+            p = new_posts.index(storage[-1]) + 1
+            # it will be problem when it bigger
+            newer = new_posts[p:]
+            storage.extend(newer)
+            save_posts(storage)
+            print(f'[App] New post detected: {len(newer)}')
+
+            for post in newer:
+                if (type(channel) == discord.channel.TextChannel):
+                    image_attached = '[글]' if post['type'] == 'icon_txt' else '[이미지]'
+                    embed = discord.Embed(
+                        title = post['subject'],
+                        description = f"{image_attached} {post['username']}",
+                        url = f"${post_url}&no={post['id']}",
+                        color = discord.Color.blue()
+                    )
+                    await channel.send(embed = embed)
+                else:
+                    # todo: Exception
+                    return
+
+            else:
+                print('[App] No new post')
 
     next_interval = random.randint(300, 600)
     print(f'[App] Next refresh: {next_interval} s')
