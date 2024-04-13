d59e6a5a2a3d3c9400582c69ca87167372fd7f0d
dayrain <blackwaterbread@gmail.com>
2024-04-13 11:35:52 +0900

    misc: delete unused

--- storage.py
+++ storage.py
@@ -1,12 +1,10 @@
 # 마지막 요소의 id가 새로 받아온 id보다 높으면 메세지 보내기
 import json
-from collections import OrderedDict
 
 path = './storage/posts.json'
 
 def load_posts():
     posts = []
-
     with open(path, 'r') as f:
         posts = json.load(f)
 
